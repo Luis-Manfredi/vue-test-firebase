67bb701eaf3f43c3bfaff1251d2bcd0289518979
Luis Manfredi <lsman2098@gmail.com>
2022-07-21 12:27:55 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,149 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Test Vue-Firebase</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-card
+        elevation="2"
+        shaped
+      >
+      
+      <v-form v-model="valid">
+        <v-container>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="user.firstname"
+              :rules="nameRules"
+              :counter="15"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="user.lastname"
+              :rules="nameRules"
+              :counter="15"
+              label="Last name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="user.email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-btn
+            type="submit"
+            @click="submit"
+          >
+          submit
+          </v-btn>
+        </v-container>
+      </v-form>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Initialization
+import { initializeApp } from 'firebase/app';
+import { getDatabase, push, ref} from 'firebase/database';
+import config from './config';
+const app = initializeApp(config);
+const db = getDatabase(app);
+let usersRef = ref(db, 'users');
 
 export default {
   name: 'App',
+
+  firebase: {
+    users: usersRef
+  },
+
   components: {
-    HelloWorld
+
+  },
+
+  data: () => ({ 
+    drawer: false,
+    valid: false,
+    user: {
+      firstname: '',
+      lastname: '',
+      email: ''
+    },
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => v.length <= 15 || 'Name must be less than 10 characters',
+    ],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+
+  methods: {
+    submit () {
+      // Send data to Firebase Realtime Database
+      push(usersRef, {
+        firstname: this.user.firstname,
+        lastname: this.user.lastname,
+        email: this.user.email
+      });
+
+      // console.log(this.user.firstname);
+      // console.log(this.user.lastname);
+      // console.log(this.user.email);
+
+      // Clear inputs
+      this.user.firstname = '';
+      this.user.lastname = '';
+      this.user.email = '';
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .v-main {
+    background-color: #393939;
+  }
+
+  .v-card {
+    width: 400px;
+    margin: 20px;
+  }
+
+  .col-md-4 {
+    max-width: none;
+  }
 </style>
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAcpbnJa7NGXuBJTdzpRt_irm0l79v1uSM",
+    authDomain: "vue-test-firebase-ad325.firebaseapp.com",
+    databaseURL: "https://vue-test-firebase-ad325-default-rtdb.firebaseio.com",
+    projectId: "vue-test-firebase-ad325",
+    storageBucket: "vue-test-firebase-ad325.appspot.com",
+    messagingSenderId: "979612624681",
+    appId: "1:979612624681:web:97181ba624d6661327f938"
+  };
